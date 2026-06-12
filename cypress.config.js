d2eda32e32e1a5d75c12621d008f73dc2335a4a8
Sean Patno <spatno@indeed.com>
2026-06-12 15:00:17 -0700

    my updates

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
+  projectId: 'a7bq2k',
   viewportWidth: 1024,
   viewportHeight: 2048,
   e2e: {
